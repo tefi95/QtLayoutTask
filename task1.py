0013a0f7b8cb51f243f54c1b7d1a78a2a1c3a6a7
tefi95 <132034111+tefi95@users.noreply.github.com>
2023-05-18 19:09:46 +1100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,22 @@
 # QtLayoutTask
+#создай тут фоторедактор Easy Editor!
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QRadioButton 
+
+app = QApplication([])
+win = QWidget()
+win.show()
+
+main_layout = QHBoxLayout() #V - вертикальная направляющая
+win.setLayout(main_layout) #устанаваливаем окну win layout - main_layout
+
+b1 = QPushButton("кто")
+main_layout.addWidget(b1)
+
+b2 = QPushButton('читает')
+main_layout.addWidget(b2)
+
+b3 = QPushButton('тот лучший')
+main_layout.addWidget(b3)
+
+app.exec_()
